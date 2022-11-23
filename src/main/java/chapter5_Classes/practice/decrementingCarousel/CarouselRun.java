4c90c2fbfc36c96a82ffee453916f7f774fb79c2
marklen <marklen86@gmail.com>
2022-11-23 15:07:04 -0600

    decrementing carousel

---
+++ src/main/java/chapter5_Classes/practice/decrementingCarousel/CarouselRun.java
@@ -0,0 +1,26 @@
+package chapter5_Classes.practice.decrementingCarousel;
+
+import java.util.Arrays;
+
+public class CarouselRun {
+
+    protected final int[] arr = DecrementingCarousel.carousel.clone();
+    protected int position = 0;
+    public int next() {
+        if (isFinished())
+            return -1;
+        else {
+            while (arr[position %= arr.length] <= 0) {
+                position++;
+            }
+        }
+        return arr[position++]--;
+    }
+    public boolean isFinished() {
+        for (int el : arr)
+            if (el > 0)
+                return false;
+        return true;
+    }
+
+}
