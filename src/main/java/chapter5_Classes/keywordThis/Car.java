38fa62bdabd8c2fedb6eafb5b5b2903c55375533
marklen <marklen86@gmail.com>
2022-11-26 20:23:40 -0600

    keyword This example

--- src/main/java/chapter5_Classes/keywordThis/Car.java
+++ src/main/java/chapter5_Classes/keywordThis/Car.java
@@ -6,31 +6,13 @@ public class Car {
     private int speed;
 
 
-
-
     public void setSpeed(int speed) {
         this.speed = speed;
 
 
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
 
-        Car car = (Car) o;
 
-        if (year != car.year) return false;
-        if (speed != car.speed) return false;
-        return model != null ? model.equals(car.model) : car.model == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = model != null ? model.hashCode() : 0;
-        result = 31 * result + year;
-        result = 31 * result + speed;
-        return result;
-    }
+   
 }
